58d836da976b97e6642096f54d53eedb16899be6
bastiendavid <bastien.david.info@gmail.com>
2015-04-18 19:50:14 +0200

    Renamed PrimeActivity.
    PrimeActivity renamed to PrimeFactorizationActivity, and moved to a
    specific package.

---
+++ app/src/main/java/com/greengames/mathgames/primefactorization/PrimeFactorizationActivity.java
@@ -0,0 +1,15 @@
+package com.greengames.mathgames.primefactorization;
+
+import com.greengames.mathgames.R;
+import com.greengames.mathgames.commons.ui.BaseActivity;
+
+import org.androidannotations.annotations.EActivity;
+
+
+/**
+ * Prime numbers decomposition game
+ */
+@EActivity(R.layout.activity_prime_factorization)
+public class PrimeFactorizationActivity extends BaseActivity {
+
+}
