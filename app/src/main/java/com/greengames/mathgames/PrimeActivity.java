cb27723797b077d69c9cf16a4689b7c30a151fdb
bastiendavid <bastien.david.info@gmail.com>
2015-04-18 19:46:44 +0200

    Project creation under Android Studio.
    Added all files needed to run a simple hello world, based on android
    annotations.

---
+++ app/src/main/java/com/greengames/mathgames/PrimeActivity.java
@@ -0,0 +1,19 @@
+package com.greengames.mathgames;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.greengames.mathgames.commons.ui.BaseActivity;
+
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.OptionsMenu;
+
+
+/**
+ * Prime numbers decomposition game
+ */
+@EActivity(R.layout.activity_prime)
+public class PrimeActivity extends BaseActivity {
+
+}
---
+++ app/src/main/java/com/greengames/mathgames/commons/ui/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.greengames.mathgames.commons.ui;
+
+import android.support.v7.app.ActionBarActivity;
+
+import org.androidannotations.annotations.EActivity;
+
+/**
+ * Base activity. Every activity of the app should inherit this activity.
+ */
+@EActivity
+public abstract class BaseActivity extends ActionBarActivity {
+}
